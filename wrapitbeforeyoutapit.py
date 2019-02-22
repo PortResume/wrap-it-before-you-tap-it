765119f6099efb5ee337b3743f72e0767f4b7a08
Skylar Barrowman <45975571+DisgruntledVet@users.noreply.github.com>
2019-02-22 09:19:54 -0500

    Update wrapitbeforeyoutapit.py

--- wrapitbeforeyoutapit.py
+++ wrapitbeforeyoutapit.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import time 
 
 #written by The DisgruntledVet
@@ -10,6 +9,16 @@ import time
 #next add what links/urls need to be checked in links.
 #next run the script in your terminal by using py wrapitbeforeyoutapit.py
 #any questions email me at whatthehacknews@gmail.com
+print ('---------------------------------')
+print ('|                                |')
+print ('|                                |')
+print ('|      WRAP IT BEFORE YOU        |')
+print ('|           TAP IT.              |')
+print ('|                                |')
+print ('---------------------------------')
+
+
+
 
 
 def lets_send_it():
@@ -17,7 +26,7 @@ def lets_send_it():
 	url = 'https://www.virustotal.com/vtapi/v2/url/scan'
 	#params is your key what is a api key it is essentially a password ro be able to use the api. 
     #url calls links which links is all the urls we want to check.
-	params = {'apikey': 'your api key', 'url': links}
+	params = {'apikey': '', 'url': links}
 	#posts the url that you want to be checked 
 	response = requests.post(url, params=params)
 
@@ -28,7 +37,7 @@ def lets_send_it():
 def give_me_that():
 	url = 'https://www.virustotal.com/vtapi/v2/url/report'
 
-	params = {'apikey': 'your api key', 'resource': links}
+	params = {'apikey': '', 'resource': links}
 	#grabs the report for the url you posted 
 	response = requests.get(url, params=params)
 
@@ -36,9 +45,45 @@ def give_me_that():
 	#prints report in terminal
 	print(response.text)
 
+	json_response = response.json()
+	#calling specific information from json. 
+	#when a json is printed off it looks like a bunch of commas and charachters 
+	#this will allow you to read the information you want to see 
+	url = json_response["url"]
+	scan_date = json_response["scan_date"]
+	positives = json_response["positives"]
+	total = json_response["total"]
+	#markdown learn about here it here https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
+	#Markdown is a lightweight markup language with plain text formatting syntax. Its design allows it to be converted to many output formats
+	md = """
+	# Scan Results
+
+	### URL: {url}
+	### SCAN DATE: {scan_date}
+	### POSITIVES: {positives}
+	###TOTAL: {total}
+	""" .format(url=url, scan_date=scan_date, positives=positives, total=total)
+	#scans = json_response['scans']
+	scanners = json_response["scans"].keys()
+	md += """
+
+	| Scanner        | Detected           | Result  |
+	| ------------- |:-------------:| -----:|
+	"""
+	for scanner in scanners:
+		detected = json_response["scans"][scanner]["detected"]
+		result = json_response["scans"][scanner]["result"]
+		md += """|{scanner}| {detected}| {result}|\n""".format(scanner=scanner, detected=detected, result=result)
+	#creating file Results.md 
+	with open("Results.md", "w") as fileObject:
+		fileObject.write(md)
+	
+
+
+
+
 #urls/links  you want to be checked 
-links = ['https://www.bing.com/',
-		 'https://www.google.com/'
+links = ['http://myportresume.com/'
 		 ]
 
 
@@ -52,7 +97,8 @@ for url in links:
    	  
 
    #this else statement posts url and gets report if status code is anything but 404 there will be a 15sec time lapse 
-   else:
+   else: 
+         
    		print ('still up', page.url)
    		lets_send_it()
    		print ('...........................grabbing report...............................')
@@ -60,5 +106,8 @@ for url in links:
    		time.sleep(15)
    		give_me_that()
    		print ('...........................posting URL..................................')
+		time.sleep(5)
+
+   
 
    		#webbrowser.open(page.url)
